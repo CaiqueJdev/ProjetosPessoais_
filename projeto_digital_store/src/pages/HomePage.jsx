4ce8299a0a49485c04fe697acb28165937dce268
CaiqueJdev <153867263+CaiqueJdev@users.noreply.github.com>
2025-09-25 11:35:48 -0300

    HomePage melhorias e adicao de novos elementos
    criacao section "colecao em destaque"

--- projeto_digital_store/src/pages/HomePage.jsx
+++ projeto_digital_store/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import { href } from "react-router-dom";
 import Layout from "../components/Layout";
 import Section from "../components/Section";
-import Img from "../assets/tenis nike.png";
 import ProductListing from "../components/ProductListing";
 import Hero from "../components/Hero";
+import YellowNike from "../assets/yellowNike.png";
+import Img from "../assets/tenis nike.png";
+import collection1 from "../assets/collection-1.png"
+import collection2 from "../assets/collection-2.png"
+import collection3 from "../assets/collection-3.png"
 const productArray = [
     {
         name:"Tenis Nike",
@@ -58,6 +62,8 @@ const HomePage = () => {
     return ( 
         <>
             <Hero/>
+
+            {/* Produtos em alta */}
             <Section
                 title={"Produtos em destaque"}
                 link={{text:"Ver todos ->", href: "/produtos"}}
@@ -67,21 +73,43 @@ const HomePage = () => {
                 <div className="m-auto flex justify-center">
                     <ProductListing products = {productArray} />
                 </div>
+
                 <Section
                     title={"Colecoes em Destaque"}
-                    titleAlign="center"
                 >
-                    <ul className="flex gap-6 p-1 bg-">
-                        <li><a href="">Camisetas</a></li>
-                        <li><a href="">Calcas</a></li>
-                        <li><a href="">Bones</a></li>
-                        <li><a href="">Headphones</a></li>
-                        <li><a href="">Tenis</a></li>
-                    </ul>
-
+                    <div 
+                        id="colection-container"
+                        className="flex jus"
+                    >
+                        <img 
+                            src={collection1} 
+                            alt="collection1" 
+                            className="rounded-sm"
+                            />
+                        <button className="bg-b1 w-[114px] h-[40px] rounded-[9px] font-bold text-white text-[14px]">
+                            
+                        </button>
+                        <img src={collection2} alt="collection2" />
+                        <img src={collection3} alt="collection3" />
+                    </div>
                 </Section>
-            </Section>
                 
+            </Section>
+
+            <Section>
+                <div className="flex">
+                    <div className="flex-5">
+                        <img src={YellowNike} alt="" />
+                    </div>
+                    <div className="flex-7">
+                        <h2>
+                            Ofertas especial
+                            </h2>
+                        <h1>air Jordan edicao de colecionador</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat corrupti hic, maxime omnis minima qui error excepturi quia voluptatum labore, exercitationem id. Impedit quos, ratione excepturi harum expedita modi!</p>
+                    </div>
+                </div>
+            </Section>
         </>
               
      );
