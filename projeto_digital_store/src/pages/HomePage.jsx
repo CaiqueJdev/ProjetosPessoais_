10a8747db150f5e344d50fd13d0252149f000df3
Lenizio27 <155769957+Lenizio27@users.noreply.github.com>
2025-09-27 09:58:13 -0300

    oferta especial create

--- projeto_digital_store/src/pages/HomePage.jsx
+++ projeto_digital_store/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Section from "../components/Section";
 import Img from "../assets/tenis nike.png";
 import ProductListing from "../components/ProductListing";
 import Hero from "../components/Hero";
+import YellowNike from "../assets/yellowNike.png"
 const productArray = [
     {
         name:"Tenis Nike",
@@ -80,6 +81,29 @@ const HomePage = () => {
                     </ul>
 
                 </Section>
+            </Section>
+            <Section>
+                <div className="flex w-full p-1">
+                    {/* Imagens */}
+                    <div className="flex relative w-full h-full flex-5 justify-center items-center">
+                        <img src={YellowNike} alt="YellowNike" className="z-1 w-full"/>
+                        <div className="absolute w-[80%] h-[80%] rounded-t-full bg-linear-to-t from-t1 to-t0"></div>
+                    </div>
+                    {/* Textos */}
+                    <div className="flex-7 p-5">
+                        <h2 className="text-[14px]">
+                            Oferta especial
+                            </h2>
+                        <h3 className="text-[48px] leading-[50px] font-bold">
+                            Air Jordan edição de colecionador
+                        </h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, aut! Ipsum consequatur accusamus enim officia, incidunt, impedit sed aperiam fuga nesciunt eius quidem suscipit beatae illum doloribus assumenda. Commodi, dolorem.
+                        </p>
+                        <button className="btHeader bg-b1 w-[114px] h-[40px] rounded-[9px] font-bold text-white text-[14px]
+                        ">Entrar</button>
+                    </div>
+                </div>
             </Section>
                 
         </>
