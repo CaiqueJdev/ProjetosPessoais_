3d72ed61c80decb02cdaefc3e7e8068e850d774c
Lenizio27 <155769957+Lenizio27@users.noreply.github.com>
2025-09-27 11:26:22 -0300

    Update HomePage.jsx

--- projeto_digital_store/src/pages/HomePage.jsx
+++ projeto_digital_store/src/pages/HomePage.jsx
@@ -3,6 +3,11 @@ import Layout from "../components/Layout";
 import Section from "../components/Section";
 import ProductListing from "../components/ProductListing";
 import Hero from "../components/Hero";
+import YellowNike from "../assets/yellowNike.png";
+import Img from "../assets/tenis nike.png";
+import collection1 from "../assets/collection-1.png"
+import collection2 from "../assets/collection-2.png"
+import collection3 from "../assets/collection-3.png"
 const productArray = [
     {
         name:"Tenis Nike",
@@ -74,29 +79,40 @@ const HomePage = () => {
                 >
                     <div 
                         id="colection-container"
-                        className="flex gap-1"
+                        className="flex jus"
                     >
-                        <div className="relative">
-                            <img
-                                src={collection1}
-                                alt="collection1"
-                                className="rounded-sm w-full h-auto"
-                                />
-                            <button 
-                                className="absolute bottom-7 left-6  bg-b1 w-[153px] h-[48px] rounded-[9px] font-bold text-white text-[14px]"
-                            >
-                                compre aqui
-                            </button>
-                            <h6 className="absolute bg-p0 text-w1 top-4 left-7 p-1 rounded-full">30% SALE</h6> 
-                        </div>
+                        <img 
+                            src={collection1} 
+                            alt="collection1" 
+                            className="rounded-sm"
+                            />
+                        <button className="bg-b1 w-[114px] h-[40px] rounded-[9px] font-bold text-white text-[14px]">
                             
+                        </button>
                         <img src={collection2} alt="collection2" />
                         <img src={collection3} alt="collection3" />
                     </div>
                 </Section>
                 
             </Section>
-                
+
+            <Section>
+                <div className="flex">
+                    <div className="flex-5">
+                        <img src={YellowNike} alt="" />
+                    </div>
+                    <div className="flex-7">
+                        <h2>
+                            Ofertas especial
+                            </h2>
+                        <h1>air Jordan edicao de colecionador</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, quaerat corrupti hic, maxime omnis minima qui error excepturi quia voluptatum labore, exercitationem id. Impedit quos, ratione excepturi harum expedita modi!</p>
+                    </div>
+                    <p>
+                        novo
+                    </p>
+                </div>
+            </Section>
         </>
               
      );
