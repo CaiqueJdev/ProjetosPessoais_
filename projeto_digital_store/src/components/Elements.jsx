d2f8cc2c70dd79f5da583e1b7ef8c5c9f1314f43
CaiqueJdev <153867263+CaiqueJdev@users.noreply.github.com>
2025-09-28 17:52:22 -0300

    Pequenas Alteracoes

--- projeto_digital_store/src/components/Elements.jsx
+++ projeto_digital_store/src/components/Elements.jsx
@@ -1,7 +1,7 @@
 const Elements = ({text}) => {
     return ( 
         <button 
-        className="bg-b1 h-[40px] rounded-[9px] font-bold text-white text-[14px] hover w-5 ">
+        className="bg-b1 h-[40px] rounded-[9px] font-bold text-white text-[14px] w-5 hover:bg-b2 transition duration-300">
                 {text}
         </button>
      );
